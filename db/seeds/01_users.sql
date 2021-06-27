c6eee5e819efb5dcb97f2e0a809c823a8ace3719
jojo2829 <joulinli@gmail.com>
2021-06-27 01:23:39 +0000

    insert for users table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users table seeds here
+
+INSERT INTO users (username, password) VALUES ('Cat', 'cat123');
+INSERT INTO users (username, password) VALUES ('Dog', 'dog123');
+INSERT INTO users (username, password) VALUES ('Patrick', 'star123');
