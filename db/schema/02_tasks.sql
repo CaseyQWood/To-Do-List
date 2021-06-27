28d1b6fa5c30ac80ce4a81fbdb13d4be25e898f4
jojo2829 <joulinli@gmail.com>
2021-06-27 01:02:20 +0000

    create tasks database table

---
+++ db/schema/02_tasks.sql
@@ -0,0 +1,10 @@
+-- Drop and recreate Tasks table
+
+DROP TABLE IF EXISTS tasks CASCADE;
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  description VARCHAR(255) NOT NULL,
+  category VARCHAR(255),
+  completed BOOLEAN NOT NULL DEFAULT FALSE
+);
