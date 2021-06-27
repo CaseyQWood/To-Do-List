26f0d7d12a72c03f58bc21e87ea47c50c27a9c83
casey.Q.Wood <caseywood252@gmail.com>
2021-06-27 11:10:00 -0700

    initial commit for homepage server file and routes

--- server.js
+++ server.js
@@ -35,11 +35,12 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const getTask = require('./routes/homePage')
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/users", usersRoutes(db));
+app.use('/', getTask(db))
 // Note: mount other resources here, using the same pattern above
 
 
