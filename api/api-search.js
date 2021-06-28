39e25edc342073732dcfb77d4c771850c107f19b
casey.Q.Wood <caseywood252@gmail.com>
2021-06-27 22:42:15 -0700

    book api initial function

--- api/api-search.js
+++ api/api-search.js
@@ -21,6 +21,25 @@ const findMovie = (userInput) => {
 // this is just for testing
 // findMovie('the grand Budapest Hotel')
 
+const findBook = (userInput) => {
+  const globalReplace = / /g;
+  const searchString = userInput.replace(globalReplace, '+');
+// YOU NEED THE API KEY FROM THE .ENV FILE FOR THIS TO WORK JUST ADD IT TO THE END OF THE URL
+  axios({
+    method: 'get',
+    url: `https://www.googleapis.com/books/v1/volumes?q=${searchString}&key=`,
+    responseType: 'json'
+  })
+  .then((res) => {
+    console.log(res.data.items[2].volumeInfo.title)    
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+};
+
+findBook('harry potter')
+
 module.exports = {
   findMovie
 };
