990b8be5165ceb54c983ecde7c387d32c477d436
casey.Q.Wood <caseywood252@gmail.com>
2021-06-29 10:44:29 -0700

    smart function base working version

--- routes/create-new.js
+++ routes/create-new.js
@@ -7,15 +7,34 @@ const {findMovie, findBook, findRestaurant} = require('../api/api-search')
 
 module.exports = () => {
   router.post("/", (req, res) => {
-    //console.log(req)
-    // const toDoItem = req.query.title
-    // const promice1 = findMovie(toDoItem)
-    // Promise.all([promice1])
-    // .then(result => {
-    //   res.json(result)
-    // })
-      res.json("hey")
-
+  
+    const toDoItem = req.body.description
+    const moviePromise = findMovie(toDoItem)
+    const bookPromise = findBook(toDoItem)
+    const restaurantPromise = findRestaurant(toDoItem)
+    Promise.all([moviePromise, bookPromise, restaurantPromise])
+    .then((result) => {
+      if (result[0]) {
+        return res.json('Movie')
+      }
+      else if (result[1]) {
+        return res.json('Book')
+      }
+      else if (result[2]){
+        return res.json('Restaurant')
+      }
+      return res.json('Product')
+    })
   })
  return router
 };
+
+async function asyncFunc() {
+  const response = await axios.get("/some_url_endpoint");
+  const data = await response.json();
+
+  return data;
+}
+
+
+ 
