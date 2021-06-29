f3467d6b00cde4bbcb80a70ac3e4e4ae7df8aa80
jojo2829 <joulinli@gmail.com>
2021-06-29 00:27:52 +0000

    change single quotes to double

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
 
   const checkIcon = function(category) {
     if (category === null) {
-      return 'fas fa-list-ul';
+      return "fas fa-list-ul";
     }
-    if (category === 'books') {
-      return 'fas fa-book';
+    if (category === "books") {
+      return "fas fa-book";
     }
-    if (category === 'films') {
-      return 'fas fa-film';
+    if (category === "films") {
+      return "fas fa-film";
     }
-    if (category === 'restaurants') {
-      return 'fas fa-utensils';
+    if (category === "restaurants") {
+      return "fas fa-utensils";
     }
-    if (category === 'products') {
-      return 'fas fa-shopping-basket';
+    if (category === "products") {
+      return "fas fa-shopping-basket";
     }
   };
 
@@ -33,26 +33,26 @@ $(document).ready(function() {
   };
 
   const renderTasks = function(tasks) {
-    $('.full-list li').remove();
+    $(".full-list li").remove();
 
     for(const task of tasks.list) {
       if (task.completed) {
-        $('.complete').prepend(createList(task));
+        $(".complete").prepend(createList(task));
       } else {
-        $('.incomplete').prepend(createList(task));
+        $(".incomplete").prepend(createList(task));
       }
     };
   };
 
   const renderCategorizedTasks = function(category, tasks) {
-    $('.full-list li').remove();
+    $(".full-list li").remove();
     for(const task of tasks.list) {
       console.log("category***: ", task.category);
       if (task.category && category === task.category) {
         if (task.completed) {
-          $('.complete').prepend(createList(task));
+          $(".complete").prepend(createList(task));
         } else {
-          $('.incomplete').prepend(createList(task));
+          $(".incomplete").prepend(createList(task));
         }
       };
     };
@@ -72,20 +72,23 @@ $(document).ready(function() {
     })
   };
 
-  $('#homes').click(function() {
+  //submit new task
+  $("form")
+
+  $("#homes").click(function() {
     loadTasks(null);
   });
-  $('#books').click(function() {
-    loadTasks('books');
+  $("#books").click(function() {
+    loadTasks("books");
   });
-  $('#films').click(function() {
-    loadTasks('films');
+  $("#films").click(function() {
+    loadTasks("films");
   });
-  $('#resturants').click(function() {
-    loadTasks('restaurants');
+  $("#resturants").click(function() {
+    loadTasks("restaurants");
   });
-  $('#products').click(function() {
-    loadTasks('products');
+  $("#products").click(function() {
+    loadTasks("products");
   });
 
   loadTasks();
