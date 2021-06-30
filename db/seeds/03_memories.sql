2340400f286f9061eea0b3d0f899fe209d6a55a9
casey.Q.Wood <caseywood252@gmail.com>
2021-06-29 23:14:01 -0700

    setup db tables and seeds for learning

---
+++ db/seeds/03_memories.sql
@@ -0,0 +1,6 @@
+INSERT INTO cortex (search_value, books, films, restaurants, products)
+VALUES ('harry potter', 5, 1, 2, 3);
+INSERT INTO cortex (search_value, books, films, restaurants, products)
+VALUES ('lord of the rings', 5, 8, 1, 4);
+
+
