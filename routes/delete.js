b8dfc0020003d96c08220c755d92c698c6f15b9b
Lana <kassembayeva@gmail.com>
2021-06-30 00:42:59 +0000

    delete route is implemented

--- routes/delete.js
+++ routes/delete.js
@@ -4,13 +4,16 @@ const router = express.Router();
 module.exports = function(db) {
   router.post('/:id', (req, res) => {
     const id = req.params.id;
-
+// console.log('req:', req)
+console.log('req.params:', req.params)
     const sql = `DELETE FROM tasks WHERE id = '${id}';`;
     db.query(sql)
     .then(result =>{
-      console.log(result)
+      res.json({success : "Updated Successfully", status : 200});
+
     });
-    return router;
+
 
   })
+  return router;
 }
