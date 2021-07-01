1b99789376f5d59691b3f7d905cdaeb46d2d23b8
casey.Q.Wood <caseywood252@gmail.com>
2021-07-01 12:58:36 -0700

    fixed back end route for marking complete or incomplete with return values to front end

--- routes/complete.js
+++ routes/complete.js
@@ -4,15 +4,31 @@ const router = express.Router();
 module.exports = (db) => {
   router.post('/:task_id', (req, res) => {
     const taskId = req.params.task_id;
-    const query = `
-    UPDATE tasks
+    const checkForStateQuery = `SELECT completed FROM tasks WHERE id  = ${taskId}`;
+    const setToFalseQuery = `UPDATE tasks
+    SET completed = FALSE Where id = '${taskId}'
+    RETURNING *;`;
+    const setToTrueQuery = `UPDATE tasks
     SET completed = TRUE Where id = '${taskId}'
     RETURNING *;`;
 
-    db.query(query)
+    console.log(taskId)
+    db.query(checkForStateQuery)
       .then((data) => {
-        const updatedItem = data.rows
-        return res.json(updatedItem)
+
+        if (data.rows[0].completed) {
+          db.query(setToFalseQuery)
+          .then(data => {
+            console.log(data.rows[0])
+            return res.json(data.rows[0])
+          })
+        } else {
+          db.query(setToTrueQuery)
+          .then(data => {
+            console.log(data.rows[0])
+            return res.json(data.rows[0])
+          })
+        }
       })
       .catch((err) => {
         console.error(err)
